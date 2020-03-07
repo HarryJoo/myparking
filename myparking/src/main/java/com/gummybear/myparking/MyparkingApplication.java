3b2e66d227c84c9bebec8cfa9a2cb7b186326d1a
Kiwi <Kiwi@DESKTOP-8B8UPVN>
2020-03-08 01:35:18 +0900

    myparking project initial commit

---
+++ myparking/src/main/java/com/gummybear/myparking/MyparkingApplication.java
@@ -0,0 +1,13 @@
+package com.gummybear.myparking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyparkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyparkingApplication.class, args);
+	}
+
+}
